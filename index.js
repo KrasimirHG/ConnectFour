969f67a39089cf86a237ab5a48b30a47260ab853
KrasimirHG <49240591+KrasimirHG@users.noreply.github.com>
2020-04-15 00:30:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+	const squares = document.querySelectorAll(".grid div");
+	console.log(squares);
+
+	const col1 = document.querySelector(".player-1");
+	const col2 = document.querySelector(".player-2");
+	// if (col1.style.backgraund - color === col2.style.backgraund - color) {
+	// 	col1.style.backgraund - color = "red";
+	// 	col2.style.backgraund - color = "blue";
+	// }
+	// console.log(col1.backgraund - color);
+};
